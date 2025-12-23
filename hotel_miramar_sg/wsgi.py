33f331f381a540e515c1e8377547dd7e38d87b18
Sammar-13 <sammarabbas9939@gmail.com>
2025-12-23 20:39:00 +0500

    chore: add vercel configuration and requirements

--- hotel_miramar_sg/wsgi.py
+++ hotel_miramar_sg/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_miramar_sg.settings')
 
 application = get_wsgi_application()
+app = application
